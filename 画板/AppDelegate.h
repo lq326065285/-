33c4c77ac7659efc0946f7e4920ee712a8800f65
liqiang <lq326065285@163.com>
2017-04-11 09:37:01 +0800

    Initial Commit

---
+++ 画板/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  画板
+//
+//  Created by 李强 on 2017/4/11.
+//  Copyright © 2017年 李强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 画板/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  画板
+//
+//  Created by 李强 on 2017/4/11.
+//  Copyright © 2017年 李强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
